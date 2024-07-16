76f964f2b69e04774716382ddde4e540c9784146
Sai_Binayak <sai0binayak99@gmail.com>
2024-07-16 22:15:04 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'nunito': ['nunito', 'sans-serif'],
+      'MyFont': ['"My Font"', 'serif'] 
+    }
+  },
+  plugins: [],
+}
+
